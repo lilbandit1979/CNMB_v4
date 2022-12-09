833e2b559a0aa856bfa44e0291c2a0f233689e65
lilbandit1979 <stephenobrien1979@Gmail.com>
2022-12-09 17:37:48 +0000

    --team class added

---
+++ Models/Team.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public enum Gender
+    {
+        boys=1,
+        girls=2
+    }
+    public enum TeamType
+    {
+        U10Football=1,
+        U11Football=2,
+        SeniorFootball=3,
+        U11Camogie=4,
+        SeniorCamogie=5,
+        U11Hurling=6,
+        SeniorHurling=7
+    }
+    public class Team
+    {
+        [Required]
+        [Key]
+        public int TeamId { get; set; }
+        [Required]
+        public  Gender Gender { get; set; }
+        [Required]
+        public TeamType TeamGame { get; set; } //football, hurling etc.
+        [Required]
+        public Teacher? Mentor { get; set; } //can be null
+    }
+}
