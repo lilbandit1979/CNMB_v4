6a85361daee5dd60cf508818f8599f0bbdeaf1e7
lilbandit1979 <stephenobrien1979@Gmail.com>
2022-12-09 16:46:06 +0000

    Add project files.

---
+++ Models/Teacher.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Teacher
+    {
+        [Required]
+        [Key]
+        public int TeacherId { get; set; }
+        [Required]
+        public string FName { get; set; } = "";
+        [Required]
+        public string SName { get; set; } = "";
+        [Required]
+        public string TeacherPhone { get; set; } = "";
+        [Required]
+        public bool IsMainRepo { get; set; }= false;
+
+        //foreign key back to School
+        public int? SchoolID { get; set; }
+
+        //useful to be able to get the school without querying the ID
+        public School? School { get; set; }
+
+    }
+}
