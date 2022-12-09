ec1716cccc38eab2fc6cba6c5715a17382569913
lilbandit1979 <stephenobrien1979@Gmail.com>
2022-12-09 16:49:56 +0000

    --Teacher and School Classes added

--- Models/School.cs
+++ Models/School.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,22 @@ namespace Models
 {
     public class School
     {
+        [Required]
+        [Key]
+        public int SchoolID { get; set; }
+        [Required]
+        public string SchoolName { get; set; } = "";
+        [Required]
+        public string SchoolPhone { get; set; } = "";
+        [Required]
+        public string SchoolAddress { get; set; } = "";
+        [Required]
+        public string SchoolEircode { get; set; } = ""; //maybe call a map API??
+
+        //list of teachers for a school
+        public virtual ICollection<Teacher>? Teachers { get; set; }
+
+
+
     }
 }
