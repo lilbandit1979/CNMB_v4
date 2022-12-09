3dbac577e4eb0ebb3ee8947171fae752f5c40633
lilbandit1979 <stephenobrien1979@Gmail.com>
2022-12-09 17:02:54 +0000

    --IRepository done

--- Repository/IRepository.cs
+++ Repository/IRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,26 @@ using System.Threading.Tasks;
 
 namespace Repository
 {
-    public class IRepository
+    public interface IRepository
     {
+        //schools
+        public IEnumerable<School> GetAllSchools();
+        
+        public School GetSchoolById(int id);
+        //public School GetSchoolByName(string name); possible
+        public void AddSchool(School school);
+        public void UpdateSchool(School school);//possibly add an id here
+        public void DeleteSchool(int id);
+
+        //teachers
+        public IEnumerable<Teacher> GetAllTeachers();
+        public Teacher GetTeacherById(int id);
+        //public Teacher GetTeacherByName(string name); //possible
+        public void AddTeacher(Teacher teacher);
+        public void UpdateTeacher(Teacher teacher); //possibly add id here as well
+        public void DeleteTeacher(int id);
+
     }
 }
+
+
