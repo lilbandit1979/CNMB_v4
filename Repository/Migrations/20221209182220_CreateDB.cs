c297514b5254f56e143b75bcd3c5e6caf8d676b4
lilbandit1979 <stephenobrien1979@Gmail.com>
2022-12-09 18:23:18 +0000

    --Database succeeded!

---
+++ Repository/Migrations/20221209182220_CreateDB.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Repository.Migrations
+{
+    public partial class CreateDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "School",
+                columns: table => new
+                {
+                    SchoolId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SchoolName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SchoolPhone = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SchoolAddress = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SchoolEircode = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_School", x => x.SchoolId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Teacher",
+                columns: table => new
+                {
+                    TeacherId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TeacherPhone = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsMainRep = table.Column<bool>(type: "bit", nullable: false),
+                    SchoolID = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Teacher", x => x.TeacherId);
+                    table.ForeignKey(
+                        name: "FK_Teacher_School_SchoolID",
+                        column: x => x.SchoolID,
+                        principalTable: "School",
+                        principalColumn: "SchoolId");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Team",
+                columns: table => new
+                {
+                    TeamId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Gender = table.Column<int>(type: "int", nullable: false),
+                    TeamGame = table.Column<int>(type: "int", nullable: false),
+                    MentorTeacherId = table.Column<int>(type: "int", nullable: false),
+                    SchoolId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Team", x => x.TeamId);
+                    table.ForeignKey(
+                        name: "FK_Team_School_SchoolId",
+                        column: x => x.SchoolId,
+                        principalTable: "School",
+                        principalColumn: "SchoolId");
+                    table.ForeignKey(
+                        name: "FK_Team_Teacher_MentorTeacherId",
+                        column: x => x.MentorTeacherId,
+                        principalTable: "Teacher",
+                        principalColumn: "TeacherId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Teacher_SchoolID",
+                table: "Teacher",
+                column: "SchoolID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Team_MentorTeacherId",
+                table: "Team",
+                column: "MentorTeacherId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Team_SchoolId",
+                table: "Team",
+                column: "SchoolId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Team");
+
+            migrationBuilder.DropTable(
+                name: "Teacher");
+
+            migrationBuilder.DropTable(
+                name: "School");
+        }
+    }
+}
